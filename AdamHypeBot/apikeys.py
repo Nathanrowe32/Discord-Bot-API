a2c274268993fe7ac100042e2f2d4cf68eef1dd2
Nathan Rowe <66985980+Nathanrowe32@users.noreply.github.com>
2021-12-10 21:45:22 -0600

    Delete apiKeys.py

--- AdamHypeBot/apikeys.py
+++
@@ -1,7 +0,0 @@
-#This file is where constants will go.
-#TOKEN is the discord bot's token.
-TOKEN = 'NIL'
-#targetID is the discord username that you want the bot to hype up.
-targetID = "NIL"
-#audio source file name
-audio_source_file = "NIL"
