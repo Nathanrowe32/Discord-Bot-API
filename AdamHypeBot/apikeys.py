9b4ab2b667aa2ee6104a656515623da3ea1afd31
Nathan Rowe <nathandrowe1@gmail.com>
2021-12-10 21:42:02 -0600

    added %commands

--- AdamHypeBot/apikeys.py
+++ AdamHypeBot/apikeys.py
@@ -1,6 +1,6 @@
 #TOKEN is the discord bot's token.
-TOKEN = 'OTE1Nzk2NTUzNzk5Njk2NDQ0.Yag0AQ.URqa4d0Dc3VvXP5_tlt1KxbD1-g'
+TOKEN = 'NIL'
 #targetID is the discord username that you want the bot to hype up.
-targetID = "Space Adam#7320"
+targetID = "NIL"
 #audio source file name
-audio_source_file = "AdamHypeTrackFinal.mp3"
+audio_source_file = "NIL"
